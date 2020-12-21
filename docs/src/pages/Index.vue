6e695f02d8a391a7b53fa347feea6e534e5bba20
Mattan Ingram <mattaningram@gmail.com>
2020-12-20 23:37:13 -0500

    Initial practical modal example

--- docs/src/pages/Index.vue
+++ docs/src/pages/Index.vue
@@ -570,7 +570,7 @@ export default {
 }
 
 .social-link {
-	--iconColor: #{primary-color(400)};
+	--icon-color: #{primary-color(400)};
 	display: inline-block;
 	vertical-align: middle;
 	margin-left: $sp-xxxs;
@@ -584,7 +584,7 @@ export default {
 	}
 
 	&:hover {
-		--iconColor: #{primary-color(700)};
+		--icon-color: #{primary-color(700)};
 	}
 }
 
